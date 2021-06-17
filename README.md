5b496e33234cab26fe5c5614f32c79ecf1e6dd40
jacksonbenete <jacksonbenete@gmail.com>
2021-06-17 23:04:01 +0000

    stc1.erl contains all chapter 1 programs

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ You need to compile `ststd.erl` first before trying any tools, as almost or all 
 
 ## File Structure
 There are redundant files.
-All the tools can be found on `stcn.erl` files, where `n` is the chapter number. So you can compile and call the stc1 module to have all chapter one tools in one place.
+All the tools can be found on `stcN.erl` files, where `N` is the chapter number. So you can compile and call the stc1 module to have all chapter one tools in one place.
 That's easier than having to compile each single tool separately.
 In those files there is a `-compile(export_all)` flag that helps in testing functions of different arities.
 
-Some files are written as escript instead, such as `echo` and `translit`. All files that are supposed to receive parameters from terminal will be written as escripts.
+Using a function from the `stcN` packages inside repl means entering the Input yourself through an arity 0 function, or passing an Input through an arity 1 function.
 
-Possibly, each separeted tool from `stcn.erl` files will be provided as separeted files as well.
+All the tools will be provided as escript as well, when possible, to accept command line arguments such as a filename, or to receive input from terminal.
---
+++ stc1.erl
@@ -0,0 +1,132 @@
+-module(stc1).
+-compile(export_all).
+
+%% copy -- copy input to output
+%%
+%% We're not defining primitives like in the book,
+%% We can't detect ENDFILE with io:get_line/1, and
+%% it already receive only one line so no need to
+%% detect ASCII NEWLINE as well.
+%%
+%% Since we can't detect ENDFILE, we can mimic ed(1)
+%% behavior so that a "." in an empty line will quit.
+copy() ->
+Input = ststd:getc(),
+copy(Input).
+
+copy(".\n") ->
+{ok, eof};
+copy(Input) ->
+ststd:putc(Input),
+copy().
+
+%% charcount -- count characters in standard input
+charcount() ->
+Input = ststd:getc(),
+charcount(Input, 0).
+
+charcount([], Acc) ->
+Acc;
+charcount([_|T], Acc) ->
+charcount(T, Acc + 1).
+
+%% linecount -- count lines in standard input
+linecount() ->
+Input = ststd:getf(),
+linecount(Input, 0).
+
+linecount([], Acc) ->
+Acc;
+% Since Erlang "String" is an array of numbers,
+% We can detect "\n" as ASCII 10.
+linecount([10|T], Acc) ->
+linecount(T, Acc + 1);
+linecount([_|T], Acc) ->
+linecount(T, Acc).
+
+%% wordcount -- count words in standard input
+%%
+%% ASCII Blank = 32, Newline = 10, Tab = 9.
+wordcount() ->
+Input = ststd:getf(),
+wordcount(Input, false, 0).
+
+wordcount([], _, Acc) ->
+Acc;
+wordcount([H|T], inword, Acc) ->
+case lists:member(H, [9, 10, 32]) of
+  true -> wordcount(T, false, Acc + 1);
+  false -> wordcount(T, inword, Acc)
+end;
+wordcount([H|T], _, Acc) ->
+case lists:member(H, [9, 10, 32]) of
+  true -> wordcount(T, false, Acc);
+  false -> wordcount(T, inword, Acc)
+end.
+
+%% putwc -- special output for wc
+%%
+%% BUGS
+%% It doesn't print the name of the file as of yet.
+putwc(ArgList) ->
+io:format(" ~w ~w ~w~n", ArgList).
+
+%% wc -- count words, lines and characters
+wc() ->
+Input = ststd:getf(),
+wc(Input, false, 0, 0, 0).
+
+wc([], _, WC, LC, CC) ->
+putwc([LC, WC, CC]);
+wc([H|T], inword, WC, LC, CC) ->
+  case lists:member(H, [9, 10, 32]) of
+    true when H =:= 10 -> wc(T, false, WC+1, LC+1, CC+1);
+    true -> wc(T, false, WC+1, LC, CC+1);
+    false -> wc(T, inword, WC, LC, CC+1)
+end;
+wc([H|T], _, WC, LC, CC) ->
+  case lists:member(H, [9, 10, 32]) of
+    true when H =:= 10 -> wc(T, false, WC, LC+1, CC+1);
+    true -> wc(T, false, WC, LC, CC+1);
+    false -> wc(T, inword, WC, LC, CC+1)
+end.
+
+%% settabs -- set initial tab stops
+%%
+%% We can't generate a list/array because we don't have a MAXLINE.
+%% settabs returns the TabSpace value instead.
+settabs() ->
+TabSpace = 4,
+TabSpace.
+
+%% tabpos -- return true if col is a tab stop
+%%
+%% Doesn't make sense in using MAXLINE since Erlang handle
+%% the EOF and buffer size for us, unless we're using
+%% io:get_chars/2 because the second parameter of the said
+%% function is exactly a limiter.
+tabpos(Col) ->
+TabSpace = settabs(),
+TabSpace - Col rem TabSpace.
+
+%% detab - convert tabs to blanks
+%%
+%% It doesn't work exactely as the Pascal version.
+%% See settabs/0 and tappos/1 for details.
+%%
+%% BUGS
+%% Special characters other than 9, 10, 32 might transform
+%% the return string in numbers and screw everything.
+detab() ->
+Input = ststd:getf(),
+detab(Input, 0, []).
+
+detab([], _, Acc) ->
+Acc;
+detab([9|T], Col, Acc) ->
+Feed = tabpos(Col),
+Blanks = lists:map(fun(_) -> 32 end, lists:seq(1, Feed)),
+detab(T, Col+Feed, lists:concat([Acc, Blanks]));
+detab([H|T], Col, Acc) ->
+detab(T, Col+1, lists:concat([Acc, [H]])).
+
